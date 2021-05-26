51988069968e3608b521a1fc04d173f7fccb8a99
Ws-Peroth <a01033056809@gmail.com>
2021-05-26 18:36:43 +0900

    add login script
    add login script

---
+++ Potato_Thief/Assets/01.Scripts/LoginTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginTest : MonoBehaviour
+{
+    // bb:91:53:67:7b:26:eb:88:5d:3d:fa:80:94:bd:37:d6:3e:a0:66:bb
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
