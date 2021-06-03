3d7929218f12d15542312edb2948e203af4b1fa6
KJG04 <66928355+KJG04@users.noreply.github.com>
2021-06-03 16:48:43 +0900

    Interaction
    add Interaction

--- Potato_Thief/Assets/01.Scripts/PlayerControl.cs
+++ Potato_Thief/Assets/01.Scripts/PlayerControl.cs
@@ -8,22 +8,30 @@ public class PlayerControl : MonoBehaviour
     //public
     public float moveSpeed = 10;
     public float jumpSpeed = 10;
+    public float skyMoveSpeed = 5;
     public KeyCode jumpKey = KeyCode.Space;
+    public GameObject joystick;
+    public KeyCode interactionKey = KeyCode.E;
 
     //private
     private Rigidbody2D _rigidbody2D;
     private bool _isPlayerJump = false;
+    private float _playerMoveSpeed;
+    private JoystickControl _joystickControl;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = transform.GetComponent<Rigidbody2D>();
+        _playerMoveSpeed = moveSpeed;
+        _joystickControl = joystick.GetComponent<JoystickControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move(moveSpeed);
+        Move(_playerMoveSpeed, Input.GetAxisRaw("Horizontal"));
+        // Move(_playerMoveSpeed, _joystickControl.GetHorizontalValue());
 
         if (Input.GetKeyDown(jumpKey) && !_isPlayerJump) //스페이스 바가 눌러졌을때와 캐릭터가 점프하고 있지 않을떄
         {
@@ -40,15 +48,26 @@ public class PlayerControl : MonoBehaviour
         }
     }
 
-    void Move(float speed)
+    void Move(float speed, float x)
     {
-        Vector2 v = new Vector2(Input.GetAxisRaw("Horizontal") * speed, _rigidbody2D.velocity.y);
+        Vector2 v = new Vector2(x * speed, _rigidbody2D.velocity.y);
         _rigidbody2D.velocity = v;
     }
 
-    void Jump(float jumpSpeed)
+    void Jump(float jumpspeed)
     {
-        Vector2 v = new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y + jumpSpeed);
+        Vector2 v = new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y + jumpspeed);
         _rigidbody2D.velocity = v;
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Interaction"))
+        {
+            if (Input.GetKeyDown(interactionKey))
+            {
+                other.gameObject.GetComponent<Interaction>().InteractionBehavior();
+            }
+        }
     }
 }
