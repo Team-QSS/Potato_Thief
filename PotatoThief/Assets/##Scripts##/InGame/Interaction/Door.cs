8e76c6eb84efa1ae34a4db099dd67f397aa06086
tyrosine1153 <tyrosine1153@gmail.com>
2021-10-15 18:09:47 +0900

    Add Scripts - Map Objects

---
+++ PotatoThief/Assets/##Scripts##/InGame/Interaction/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InGame
+{
+    public class Door : Obstacle
+    {
+        private SpriteRenderer spriteRenderer;
+
+        private readonly Color activeColor = Color.green;
+        private readonly Color inactiveColor = Color.white;
+
+        private void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        public override void ActivateObstacle()
+        {
+            spriteRenderer.color = activeColor;
+        }
+
+        public override void DeactivateObstacle()
+        {
+            spriteRenderer.color = inactiveColor;
+        }
+    }
+}
---
+++ PotatoThief/Assets/##Scripts##/InGame/Interaction/Lever.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InGame
+{
+    public class Lever : Trigger
+    {
+        private SpriteRenderer spriteRenderer;
+
+        private readonly Color activeColor = Color.green;
+        private readonly Color inactiveColor = Color.white;
+
+        private void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        protected override void ActivateTrigger()
+        {
+            spriteRenderer.color = activeColor;
+        }
+
+        protected override void DeactivateTrigger()
+        {
+            spriteRenderer.color = inactiveColor;
+        }
+    }
+}
