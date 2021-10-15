296c8a67399adea2f50ee1551f22502b91a9bc3f
tyrosine1153 <tyrosine1153@gmail.com>
2021-10-15 18:05:42 +0900

    Add Scripts - Interactions

---
+++ PotatoThief/Assets/##Scripts##/InGame/Interaction/Interaction.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InGame
+{
+    // 상호작용 - Obstacle, Trigger가 상속받음
+    public class Interaction : MonoBehaviour
+    {
+        public bool Status { get; set; }
+    }
+}
---
+++ PotatoThief/Assets/##Scripts##/InGame/Interaction/Repeater.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace InGame
+{
+    public class Repeater : MonoBehaviour
+    {
+        [SerializeField] private List<Trigger> triggers;
+        [SerializeField] private List<Obstacle> obstacles;
+
+        private void Start()
+        {
+            // 리피터 등록
+            foreach (var trigger in triggers)
+            {
+                trigger.Repeater = this;
+            }
+        }
+
+        // 전체 트리거 확인
+        public void TriggerStatusCheck()
+        {
+            if (triggers.Any(trigger => !trigger.Status))
+            {
+                DeactivateObstacles();
+                return;
+            }
+
+            ActivateObstacles();
+        }
+
+        // 전체 장애물 활성화
+        private void ActivateObstacles()
+        {
+            foreach (var obstacle in obstacles)
+            {
+                obstacle.ActivateObstacle();
+                obstacle.Status = true;
+            }
+        }
+
+        // 전체 장애물 비활성화
+        private void DeactivateObstacles()
+        {
+            foreach (var obstacle in obstacles)
+            {
+                obstacle.DeactivateObstacle();
+                obstacle.Status = false;
+            }
+        }
+    }
+}
