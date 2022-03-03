098b522bf63da98475c293b3491c943a002ac89e
Ws-Peroth <a01033056809@gmail.com>
2022-03-03 13:01:36 +0900

    Fix: Setting Panel UI

--- PotatoThief/Assets/##Scripts##/UI/SettingsPanel.cs
+++ PotatoThief/Assets/##Scripts##/UI/SettingsPanel.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class SettingsPanel : MonoBehaviour
     {
+        [SerializeField] private Slider bgmSlider;
+        [SerializeField] private Slider sfxSlider;
+        public void OnEnable()
+        {
+            bgmSlider.value = AudioManager.Instance.BGMVolume;
+            sfxSlider.value = AudioManager.Instance.SFXVolume;
+        }
+
         public void OnBGMSliderValueChanged(float value)
         {
             AudioManager.Instance.BGMVolume = value;
